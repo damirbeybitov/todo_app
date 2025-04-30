2552f2e9e67f2c537b6a4511c811351a612a204c
damirbeybitov <damirbey7@gmail.com>
2025-04-30 16:49:08 +0500

    Исправлено использование модели User на модель Task в CRUDTask для корректной работы с задачами.

--- app/crud/crud_task.py
+++ app/crud/crud_task.py
@@ -1,9 +1,9 @@
 # app/crud/crud_user.py
 from app.crud.base import CRUDBase
-from app.models.task import User
+from app.models.task import Task
 from app.schemas.task import TaskCreate, TaskUpdate
 
-class CRUDTask(CRUDBase[User, TaskCreate, TaskUpdate]):
+class CRUDTask(CRUDBase[Task, TaskCreate, TaskUpdate]):
     pass
 
-task_crud = CRUDTask(User)
+task_crud = CRUDTask(Task)
